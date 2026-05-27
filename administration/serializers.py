fa4fa30142d57e94678fb75d495c7a120d317352
KohakuC <60190850+KohakuC@users.noreply.github.com>
2026-05-27 15:53:34 +0200

    General comments added

--- administration/serializers.py
+++ administration/serializers.py
@@ -3,6 +3,15 @@ from rest_framework import serializers
 
 User = get_user_model()
 
+"""
+Serializers for the administrator app.
+
+A serializer converts Django model instances to JSON format (and vice versa),
+and handles data validation before saving to the database.
+
+This module contains:
+- AdminUserListSerializer: serializes compact user's information for the admin list view
+"""
 
 class AdminUserListSerializer(serializers.ModelSerializer):
     """Compact representation of a user for the admin list view."""
